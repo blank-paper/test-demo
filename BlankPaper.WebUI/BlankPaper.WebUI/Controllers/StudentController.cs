940c75c91d198f4d458c443a1aed0075a243154e
Alan.Luo <iteamalan@hotmail.com>
2019-04-22 22:26:20 +0800

    实验四 Demo

---
+++ BlankPaper.WebUI/BlankPaper.WebUI/Controllers/StudentController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Demo.WebUI.Models;
+
+namespace Demo.WebUI.Controllers
+{
+    public class StudentController : Controller
+    {
+        /// <summary>
+        /// 显示学生主页列表信息
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Index(string name, int age)
+        {
+            //访问数据库获取一个学生列表
+
+            var students = new List<Student>() {
+                new Student(){ Id = 10001,Name="Alan",Age=34,Remark="Teacher" },
+                new Student(){ Id = 10002,Name="Tom",Age=18,Remark="Student" },
+                new Student(){ Id = 10003,Name="Jerry",Age=19,Remark="Student" }
+            };
+            //ViewData["Students"] = students;
+
+            return View("Index", students);
+        }
+        /// <summary>
+        /// 显示学生明细信息
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Detail(int id)
+        {
+            return View(id);
+        }
+    }
+}
---
+++ BlankPaper.WebUI/BlankPaper.WebUI/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Demo.WebUI.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Remark { get; set; }
+    }
+}
+
